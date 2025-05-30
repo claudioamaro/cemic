9d06b968e1e12c7c27a6d3a443efe0f64f1d2225
Salvador Bila <salvador.bila@tistech.co.ao>
2025-05-30 21:39:59 +0100

    estrutura base

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'CEMIC (Odoo 18)',
+    'version': '18.0.1.0.0',
+    'summary': 'Módulo customizado para CEMIC.',
+    'author': '',
+    'website': '',
+    'category': 'extra',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'application': True, # Marque como True se for uma aplicação principal
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
